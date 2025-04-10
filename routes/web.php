eedd7a5ec9a28ee6ad2901cc8100d1b11c5d6bd2
[adityarn] <[adityarizkiawann@gmail.com]>
2025-04-10 22:03:01 +0700

    Updating Template & JS Laravel

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+// Route::get('/home', function () {
+//     return view('home');
+// });
 
 Route::get('/about', function () {
     return view('about');
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
 });

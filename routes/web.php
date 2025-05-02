d06f4dd4f52e00f1661e9ee504f7d25da366bc94
[adityarn] <[adityarizkiawann@gmail.com]>
2025-05-02 20:57:53 +0700

    Update Layout, Template & Blade Component Laravel

--- routes/web.php
+++ routes/web.php
@@ -3,21 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', ['title' => 'Home Page']);
 });
 
-// Route::get('/home', function () {
-//     return view('home');
-// });
-
 Route::get('/about', function () {
-    return view('about');
+    return view('about', ['title' => 'About Page']);
 });
 
 Route::get('/blog', function () {
-    return view('blog');
+    return view('blog', ['title' => 'Blog Page']);
 });
 
 Route::get('/contact', function () {
-    return view('contact');
+    return view('contact', ['title' => 'Contact Page']);
 });
